2707c46edace09919a49ffeff52deb7b9bc78f8e
chumples <84584789+chumples@users.noreply.github.com>
2021-06-07 11:34:42 +0700

    solution

---
+++ main.js
@@ -0,0 +1,25 @@
+const p = document.createElement('p');
+p.textContent = 'Hey I\'m red!';
+p.style.cssText = 'color: red';
+document.getElementById('container').appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.textContent = 'I\'m a blue h3!';
+h3.style.cssText = 'color: blue';
+document.getElementById('container').appendChild(h3);
+
+const div = document.createElement('div');
+div.style.border = 'black';
+div.style.backgroundColor = 'pink';
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I\'m in a div';
+
+div.appendChild(h1);
+
+const p2 = document.createElement('p2');
+p2.textContent = 'ME TOO!';
+
+div.appendChild(p2);
+
+container.appendChild(div)
