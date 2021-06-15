ad9d9d49b042a03a28e7707b0a81ef1aab0b36bc
chumples <84584789+chumples@users.noreply.github.com>
2021-06-15 11:53:25 +0700

    changes

--- main.js
+++ main.js
@@ -30,5 +30,17 @@ const btn = document.querySelector('#btn');
 btn.onclick = () => alert('Hello World');*/
 
 const btn = document.querySelector('#btn');
+
+
+
+/*btn.addEventListener('click', function (e) {
+    console.log(e);
+  });*/
+
 btn.addEventListener('click', () => 
     {alert('Hello World'); } );
+
+    btn.addEventListener('click', function (e) {
+        e.target.style.background = 'blue';
+      });
+      
